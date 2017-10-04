0e21be78ebcc5d39fd98aa7e934d75fc3d0031ca
Rich Moyse <rich@moyse.us>
2017-10-04 13:33:47 -0400

    eliminate heredoc, closure functions, define format for vendor.config file

--- config.sh
+++ config.sh
@@ -1,52 +1,62 @@
-
-#config_vendor(){
-#cat <<ConfigSettingsMyself
-#'component/pathset'  'git@github.com:WhisperingChaos/BuildConfig.git' 'master'  "$(dirname ${BASH_SOURCE[0]})"
-#ConfigSettingsMyself
-#}
-
-config_recur(){
+config_vendor_file_search(){
 	local -r parentPath="$1"
+
 	local configPath
 	local subDir
 	while read -r configPath; do
 		local vendorDir="$(dirname $configPath)"
-		cat  "$configPath" | config_recur_vendor_path_append "$vendorDir"
+
+		config_vendor_file_echo "$configPath"
+		if cat $configPath | config_vendor_file; then 
+			cat $configPath | config_vendor_file_entries | config_vendor_path_append "$vendorDir"
+		fi
+
 		for subDir in $(ls -d "$vendorDir/*/" 2>/dev/null); do
-			config_recur "$subDir"
+			config_vendor_file_search "$subDir"
 		done
-	done < <( ls "$parentPath/vendor.config" )
+	done < <( $ls "$parentPath/vendor.config" 2>/dev/null )
 }
 
-config_recur_vendor_path_append(){
+config_vendor_file(){
+
+	local config
+	while read -r config; do
+		if [[ $config ~= ^[:space:]*#<vendor\.config:(alnum][.-])+>$ ]]; then return true; fi
+	done
+	return false
+}
+
+config_vendor_file_entries(){
+
+	local config
+	while read -r config; do
+		if [[ $config ~= ^[:space:]*# ]]; then continue; fi
+		if [[ $config ~= ^[:space:]*$ ]]; then continue; fi
+		echo "$config"
+	done
+}
+
+config_vendor_path_append(){
 	local -r vendorDir=$1
+
+	local config
 	while read -r config; do
 		echo "$config $vendorDir"
 	done
 }
+
 config_myself(){
 	local -r parentPath="$1"
-#	config_myself_vendor "$parentPath"
-#	config_setting_iterate 'config_myself_vendor_closure'
-	config_recur_start "$parentPath"
-	config_setting_iterate 'config_recur_start_closure'
-}
-config_myself_vendor(){
-	eval config_myself_vendor_closure\(\)\{ local \-r start\=\'$1\'  \; config_recur \"\$start\" \; \}
-
+	config_vendor_file_search "$parentPath" | config_setting_iterate
 }
 
 config_setting_iterate(){
-	local -r sourceFn="$1"
 
+	local entry
 	while read -r entry; do
 		eval set -- $entry
 		config_install "$1" "$2" "$3" "$4"
-	done < <( $sourceFn )
-}
-
-config_recur_start(){
-	eval config_recur_start_closure\(\)\{ local \-r start\=\'$1\'  \; config_recur \"\$start\" \; \}
+	done
 }
 
 config_install(){
@@ -61,21 +71,22 @@ config_install(){
 }
 
 config_compose(){
-
+	# minimally compose myself
 	local -r myRoot="$(dirname ${BASH_SOURCE[0]})"
 	config_myself "$myRoot"
-
+	# now fully compose myself
 	for mod in $( "$myRoot/composer/include.composer.sh" "$myRoot"); do
 		source "$mod"
 	done
 
-	path_Set "${myRoot}" 
+# path_Set "${myRoot}" 
 }
 
 main_call(){
 	echo "path $1"
 }
 set -ex
-# config must first compose a minimal self then continue composing others.
+# compose myself
 config_compose
-main_call
+# compose others
+# main_call

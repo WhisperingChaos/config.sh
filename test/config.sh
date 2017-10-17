e1e13af94a8d2cb1d3a30e480c4f21d6442a916b
Rich Moyse <rich@moyse.us>
2017-10-17 10:42:49 -0400

    add boilerplate code to compose testing script

--- test/config.sh
+++ test/config.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
-source "$(dirname ${BASH_SOURCE[0]})"/config.include.sh
-source "$(dirname ${BASH_SOURCE[0]})"/assert.include.sh
+config_executeable(){
+	local -r myRoot="$1"
+	# include components required to create this executable
+	local mod
+	for mod in $( "$myRoot/composer/include.composer.sh" "$myRoot"); do
+		source "$mod"
+	done
+	# include component to be tested 
+	source "$myRoot/config.include.sh"
+}
+
 test_config_vendor_file_banner_detected(){
 	assert_true "echo '#<vendor.config:1.0>' | config_vendor_file"
 	assert_true 'test_config_vendor_file_banner_white_space | config_vendor_file'
 	assert_true 'test_config_vendor_file_banner_detect_anywhere | config_vendor_file'
 	assert_false "echo '#<vendor.config:.0>' | config_vendor_file"
 	assert_false "echo 'bad#<vendor.config:.0>' | config_vendor_file"
-
 }
+
 test_config_vendor_file_banner_white_space(){
 	echo ' 	#<vendor.config:1.0>' 
 }
@@ -51,7 +60,10 @@ col1.1 col2.1 col3.1 /vendor
 vendor_path_parents
 }	
 
+config_executeable  "$(dirname "${BASH_SOURCE[0]}")" 
+
 # invoke tests
+
 test_config_vendor_file_banner_detected
 test_config_vendor_file_entries
 test_config_vendor_path_append

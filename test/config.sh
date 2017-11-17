d25e98cc985ee5967fcd3c69e21c970f08aa6af9
Rich Moyse <rich@moyse.us>
2017-11-17 18:40:43 -0500

    more tests

--- test/config.sh
+++ test/config.sh
@@ -30,28 +30,58 @@ test_config_vendor_banner_not_first(){
 }
 
 test_config_vendor_read(){
-	local pasTempFileNm
-	test_config_vendor_temp_file_create test_config_vendor_file_empty 'pasTempFileNm'
-	assert_true  "echo '$pasTempFileNm' | config_vendor_read | assert_output_true  test_config_vendor_file_vendorDir '$pasTempFileNm'" 
-}
-#test_config_vendor_file_stream(){
-#	local -r vendorGen="$1"
-#	local rtnTempFileNm
-#	test_config_vendor_temp_file_create "$vendorGen" 'pasTempFileNm'
-#}
+	assert_true "test_config_vendor_temp_file_reading test_config_vendor_file_empty test_config_vendor_file_empty"
+	assert_true "test_config_vendor_temp_file_reading test_config_vendor_file_one_entry test_config_vendor_file_one_entry_output"
+	assert_true "test_config_vendor_temp_file_reading test_config_vendor_file_two_entry test_config_vendor_file_two_entry_output"
+ }
+test_config_vendor_temp_file_reading(){
+	local -r vendorGen="$1"
+	local -r vendorGenOut="$2"
 
+	local pasTempFileNm
+	test_config_vendor_temp_file_create "$vendorGen" 'pasTempFileNm'
+	test_vendorGenOut_wrapper(){
+		$vendorGenOut "$pasTempFileNm"
+	}
+	echo "$pasTempFileNm" | config_vendor_read | assert_output_true test_vendorGenOut_wrapper
+
+	local -i returnCd=$?
+	local -r tmpDirPrefix='/tmp/' 
+	if [ "${pasTempFileNm:0:${#tmpDirPrefix}}" == "$tmpDirPrefix" ]; then
+		rm -rf $( dirname "$pasTempFileNm" )
+		(( returnCd = ( returnCd + $? )	))
+	fi
+	return $returnCd
+}
 test_config_vendor_temp_file_create(){
 	local -r vendorGen="$1"
 	local -r rtnTempFileNm="$2"
-	local -r tmpVendor=$(mktemp --tmpdir test_config_vendor.XXXXXXXXXX)'/vendor.config'
+	local -r tmpVendor=$(mktemp --tmpdir -d test_config_vendor.XXXXXXXXXX)'/vendor.config'
 	assert_true "'$vendorGen' > '$tmpVendor'"
-	eval $rtnTempFileNm=tmpVendor
+	eval $rtnTempFileNm=\"\$tmpVendor\"
 }
 test_config_vendor_file_empty(){
+	return
 }
-test_config_vendor_file_vendorDir(){
+test_config_vendor_file_one_entry(){
+	echo "column1 column2 colum3"
+}
+test_config_vendor_file_one_entry_output(){
+	test_config_vendor_file_mark_gen "$1"
+	test_config_vendor_file_one_entry
+}
+test_config_vendor_file_two_entry(){
+	test_config_vendor_file_one_entry
+	test_config_vendor_file_one_entry
+}
+test_config_vendor_file_two_entry_output(){
+	test_config_vendor_file_mark_gen "$1"
+	test_config_vendor_file_one_entry
+	test_config_vendor_file_one_entry
+}
+test_config_vendor_file_mark_gen(){
 	local vendorFileNm="$1"
-	echo "$config_VENDOR_FILE_SCOPE_MARK unset vendorDir; local -r vendorDir='$(dirname "$vendorFileNm")'"
+	echo "$config_VENDOR_FILE_SCOPE_MARK"'unset vendorDir; local -r vendorDir='"'$(dirname $vendorFileNm)'"';'
 }
 
 test_config_vendor_file_entries(){
@@ -62,18 +92,31 @@ test_config_vendor_file_entries(){
 test_config_vendor_file_entries_single_line(){
 	echo 'column1 column2 colum3'
 }
+test_config_vendor_whitespace_exclude(){
+	assert_true 'test_config_vendor_file_entries_single_line_comments | config_vendor_whitespace_exclude | assert_output_true "exit 0"'
+	#set -x
+	assert_true 'test_config_vendor_file_entries_marks_allow | config_vendor_whitespace_exclude | assert_output_true test_config_vendor_file_entries_marks_allow_output'
+}
 test_config_vendor_file_entries_single_line_comments(){
 	echo '#<vendor.config:1.0>'
-	test_config_vendor_file_entries_single_line
 	echo '# my comment'
 	echo '# another comment'
+	echo '         '
 }
+test_config_vendor_file_entries_marks_allow(){
+	test_config_vendor_file_mark_gen 'testdir/testfile'
+	echo "${config_VENDOR_BASH_SNIPPETS_MARK}local -r variableName='value'"
+}
+test_config_vendor_file_entries_marks_allow_output(){
+	echo "1 $(test_config_vendor_file_mark_gen 'testdir/testfile')"
+	echo "2 ${config_VENDOR_BASH_SNIPPETS_MARK}local -r variableName='value'"
+}
+
 
 test_config_vendor_path_append(){
 	assert_true 'test_config_vendor_path_parents | config_vendor_path_append "/vendor" | assert_output_true test_config_vendor_path_parents_with_vendor'
 	assert_true 'test_config_vendor_path_parents_quoted | config_vendor_path_append "/vendor" | assert_output_true test_config_vendor_path_parents_with_vendor_quoted'
 }	
-
 test_config_vendor_path_parents(){
 cat <<vendor_path_parents
 col1 col2 col3
@@ -195,6 +238,8 @@ main(){
 	# invoke tests
 #	test_config_section_found
 	test_config_vendor_banner_detected
+	test_config_vendor_read
+	test_config_vendor_whitespace_exclude
 #	test_config_vendor_file_entries
 #	test_config_vendor_path_append
 #	test_config_component_part

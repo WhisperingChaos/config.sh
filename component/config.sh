5f603db84a5196f0565f5526cc0dc031e50675da
Rich Moyse <rich@moyse.us>
2017-12-07 15:16:47 -0500

    change dir reference to align with component semantics

--- component/config.sh
+++ component/config.sh
@@ -2,7 +2,7 @@
 # separate functions so they can be tested but due
 # to bootstrap constraint, include must be in same
 # physical directory as this command (component).
-source "$(dirname "${BASH_SOURCE[0]}")"/config.include.sh
+source "$(dirname "${BASH_SOURCE[0]}")"/base/config.include.sh
 
 main(){
 	local -r rootDir="$1"
@@ -14,7 +14,7 @@ main(){
 	fi
 	# now fully compose myself because others are using me to
 	# compose themselves
-	for mod in $( "$myRoot/composer/composer.sh" "$myRoot"); do
+	for mod in $( "$myRoot/composer.sh/composer.sh" "$myRoot"); do
 		source "$mod"
 	done
 	# config all components rooted in this tree

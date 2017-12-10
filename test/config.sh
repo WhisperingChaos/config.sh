f433cba73dce2e7baae810fc4454c9cc7d993144
Rich Moyse <rich@moyse.us>
2017-12-09 22:01:05 -0500

    make sure its a link - not actual file

--- test/config.sh
+++ test/config.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-main(){
-	local -r projRoot="$1"
-
-	set -e
-	local -r repo='https://github.com/WhisperingChaos/config.sh' 
-	local -r ver='master' 
-	local -r repoVerUrl="$repo"'/tarball/'"$ver"
-	local -r configDir='./config' 
-	if ! [ -d "$configDir" ]; then 
-		mkdir $configDir
-	fi
-	wget --dns-timeout=5 --connect-timeout=10 --read-timeout=60 -O -  "$repoVerUrl" \
-	| tar -xz -C $configDir --strip-component=2 --wildcards --no-wildcards-match-slash --anchor '*/component/'
-	$configDir/config.sh 
-}
-main "`pwd`"
+../config.sh

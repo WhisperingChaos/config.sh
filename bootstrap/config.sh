6a4891b157d98ab60896a795f16dda952b332861
Rich Moyse <rich@moyse.us>
2020-03-13 21:06:14 -0400

    select defaults that encourage user to use default value, order paramaters to correspond to likely overridding sequence, display errors for wget

--- bootstrap/config.sh
+++ bootstrap/config.sh
@@ -5,41 +5,41 @@
 #		Bootstrap mechanism to download and run config.sh.
 #
 #	In
-#		$1 - (Required) Directory containing or symbolic link to this script. 
-#		$2 - (Optional) Version of config.sh to use. Defaults to 'master'
-#		$3 - (Optional) Directory path to vendor fiile.  Defaults to
-#		     'config_sh/vendor' located in $3.
-#		$4 - (Optional) Install directory path for config.sh. Defaults to a 
+#		BASH_SOURCE[0] - (Required) Directory containing or symbolic link to this script. 
+#		$1 - (Optional) Version of config.sh to use. Defaults to 'master'
+#		$2 - (Optional) Install directory path for config.sh. Defaults to a 
 #		     subdirectory called 'config_sh' whose parent is the directory
 #		     containing the script or directory from a symbolic link.  If the
 #		     install doesn't exist, its created.
+#		$3 - (Optional) Directory path to vendor fiile.  Defaults to
+#		     "$2/vendor".
 #
 ###############################################################################
 main(){
-	local -r bootRoot="$(dirname "$1")"
-	local -r configVer="${2:-master}"
-	local 	 vendorPath="$3"
-	local    installPath="$4"
+	local -r bootRoot="$(dirname "${BASH_SOURCE[0]}")"
+	local -r configVer="${1:-master}"
+	local 	 vendorPath="$2"
+	local    installPath="$3"
 
 	if [[ -z "$installPath" ]]; then 
 		installPath="$bootRoot/config_sh"
 	fi
 
 	if ! [[ -d "$installPath" ]]; then 
-		mkdir "$installPath"
+		mkdir -p "$installPath"
 	fi
 
 	if [[ -z "$vendorPath" ]]; then
-		vendorPath="$installPath"
+		vendorPath="$installPath/vendor"
 	fi
 
 	local -r repoVerUrl='https://github.com/WhisperingChaos/config.sh/tarball/'"$configVer"
-	wget --dns-timeout=5 --connect-timeout=10 --read-timeout=60 -O -  "$repoVerUrl" 2>/dev/null \
+	wget --dns-timeout=5 --connect-timeout=10 --read-timeout=60 -O -  "$repoVerUrl" \
 	| tar -xz -C $installPath --strip-component=2 --wildcards --no-wildcards-match-slash --anchor '*/component/'
 
 	"$installPath/config.sh" "$vendorPath" 
 }
 
 set -e
-main "${BASH_SOURCE[0]}" "$@"
+main "$@"
 

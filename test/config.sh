1c383ec6368639e40b979392e248f50b94733df5
Rich Moyse <rich@moyse.us>
2020-03-16 15:31:32 -0400

    configure components needed by bootstrap test.

--- test/config.sh
+++ test/config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "info: Assumes current working directory version of config.sh in config.sh repository works!"
 ../component/config.sh ./config_test_sh/vendor
+../component/config.sh ./config_boot_test_sh/vendor
